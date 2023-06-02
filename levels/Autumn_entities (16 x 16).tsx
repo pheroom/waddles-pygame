d9358bf6cf6e218412bcbb0390bfa255e46af495
qerenny <89642064+qerenny@users.noreply.github.com>
2023-06-02 03:14:40 +0300

    Files for map

---
+++ levels/Autumn_entities (16 x 16).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Autumn_entities (16 x 16)" tilewidth="32" tileheight="32" tilecount="30" columns="10">
+ <image source="2 - Autumn Forest/Autumn_entities (16 x 16).png" width="320" height="112"/>
+</tileset>
---
+++ levels/tileset_night.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tileset_night" tilewidth="32" tileheight="32" tilecount="128" columns="8">
+ <image source="C:/Users/ender/OneDrive/Рабочий стол/bitmaps/tileset_night.png" width="256" height="512"/>
+</tileset>
