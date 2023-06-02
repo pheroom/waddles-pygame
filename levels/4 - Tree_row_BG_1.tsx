3b7b25b23b250f98ec9ffc3069d42ebdc24d2dff
qerenny <89642064+qerenny@users.noreply.github.com>
2023-06-02 10:14:43 +0300

    Got rid of garbage and added the missing png

--- levels/4 - Tree_row_BG_1.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="4 - Tree_row_BG_1" tilewidth="32" tileheight="32" tilecount="54" columns="9">
- <image source="2 - Autumn Forest/Background parts/4 - Tree_row_BG_1.png" width="288" height="208"/>
-</tileset>
--- levels/Blocks (16 x 16).tsx
+++ levels/Blocks (16 x 16).tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="Blocks (16 x 16)" tilewidth="32" tileheight="32" tilecount="15" columns="5">
- <image source="5 - Misc. universal tiles/Blocks (16 x 16).png" width="176" height="96"/>
+ <image source="Blocks (16 x 16).png" width="176" height="96"/>
 </tileset>
